93a647c1ddddfc449f840f446eb1742a5876d1c7
lambcount <53368935+lambcount@users.noreply.github.com>
2022-11-24 16:49:03 +0100

    Initial commit of SciNote API for Julia

---
+++ src/SciNote.jl
@@ -0,0 +1,29 @@
+__precompile__()
+module SciNote
+
+include("requests.jl")
+
+export 
+    check_vpn,
+    api_running,
+    token,
+    header,
+    get_all_teams,
+    get_projects,
+    get_experiments,
+    get_tasks,
+    get_protocols,
+    get_steps,
+    get_attachments,
+    get_inventories,
+    get_inventoryitems,
+    get_inventorycolumns,
+    list_inventories,
+    list_items,
+    list_experiments,
+    list_tasks,
+    list_steps,
+    get_sample,
+    post_step
+
+end 
